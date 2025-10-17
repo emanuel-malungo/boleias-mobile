a3ef990e853e930f5dde7ab5dcadee7cdfae0bef
emanuel-malungo <emanuelmalungo856@gmail.com>
2025-10-17 09:26:18 +0100

    Refactor code structure for improved readability and maintainability

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,41 @@
-import { Text, View } from "react-native";
-import Splash from "@/components/layout/Splash";
+import icon from '@/assets/images/icon.png';
+import driving from '@/assets/images/driving.png';
+import { View, Text, Image, StatusBar, TouchableOpacity } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <SafeAreaView
+      className='flex-1 w-full justify-center items-center bg-gray-100'
     >
-      <Splash />
-    </View>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="#f3f4f6"
+      />
+
+      <Image source={icon} />
+
+      <Image source={driving} />
+
+      <View>
+        <Text className='text-2xl font-semibold text-center mt-4'>
+          Bem-vindo ao Boleias 🚘
+        </Text>
+        <Text className='text-base text-center mt-2 px-4'>
+          Encontre boleias, partilhe viagens e {'\n'}
+          chegue onde precisa de forma {'\n'}
+          simples e segura.
+        </Text>
+      </View>
+
+      <TouchableOpacity
+        className='bg-[#2586D9] px-6 py-3 rounded-full mt-6 w-full max-w-xs justify-center items-center'
+      >
+        <Text className='text-white text-lg font-medium'>
+          Começar
+        </Text>
+      </TouchableOpacity>
+
+    </SafeAreaView>
   );
 }
